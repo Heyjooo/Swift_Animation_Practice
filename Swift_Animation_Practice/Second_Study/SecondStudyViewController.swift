e54fa2d5008cf58eb278902b54c98ffdc44704ec
heejoo <heejoo_byun@naver.com>
2023-11-13 13:08:52 +0900

    [Chore] 2주차 스터디 setting

---
+++ Swift_Animation_Practice/Second_Study/SecondStudyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondStudyViewController.swift
+//  Swift_Animation_Practice
+//
+//  Created by 변희주 on 2023/11/13.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SecondStudyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    private func setUI() {
+    }
+}
