c11cc8d1203eb2523525e49d77d01cb7aa47836a
heejoo <heejoo_byun@naver.com>
2023-11-24 12:15:34 +0900

    [Chore] 3주차 스터디 세팅

---
+++ Swift_Animation_Practice/Third_Study/ThirdStudyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdStudyViewController.swift
+//  Swift_Animation_Practice
+//
+//  Created by 변희주 on 2023/11/20.
+//
+
+import UIKit
+
+final class ThirdStudyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
