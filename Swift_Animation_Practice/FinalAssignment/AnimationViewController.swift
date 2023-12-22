bc175f3c36797c13c65a20d2ce68f288359faeb6
heejoo <heejoo_byun@naver.com>
2023-12-23 00:27:47 +0900

    [Feat] Lottie 및 Label 추가

--- Swift_Animation_Practice/FinalAssignment/AnimationViewController.swift
+++ Swift_Animation_Practice/FinalAssignment/AnimationViewController.swift
@@ -13,6 +13,8 @@ final class AnimationViewController: UIViewController {
     var timer: Timer?
     var progressViews: [UIProgressView] = []
     var currentIndex: Int = 0
+    private let mainStrings: [String] = [Strings.mainString.first, Strings.mainString.second, Strings.mainString.third, Strings.mainString.fourth]
+    private let subStrings: [String] = [Strings.subString.first, Strings.subString.second, Strings.subString.third, Strings.subString.fourth]
     
     private let progressStackView: UIStackView = {
         let stackView = UIStackView()
@@ -32,6 +34,21 @@ final class AnimationViewController: UIViewController {
         return button
     }()
     
+    private let mainString: UILabel = {
+       let label = UILabel()
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 28)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let subString: UILabel = {
+       let label = UILabel()
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,6 +63,8 @@ final class AnimationViewController: UIViewController {
     
     private func setLayout() {
         view.addSubviews(progressStackView,
+                         mainString,
+                         subString,
                          nextButton)
         
         for _ in 0..<4 {
@@ -63,6 +82,16 @@ final class AnimationViewController: UIViewController {
             $0.height.equalTo(3)
         }
         
+        mainString.snp.makeConstraints {
+            $0.top.equalTo(progressStackView.snp.bottom).offset(40)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        subString.snp.makeConstraints {
+            $0.top.equalTo(mainString.snp.bottom).offset(10)
+            $0.leading.equalTo(mainString)
+        }
+        
         nextButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(70)
             $0.leading.trailing.equalToSuperview().inset(15)
@@ -75,6 +104,13 @@ final class AnimationViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 0.0001, target: self, selector: #selector(setProgress), userInfo: nil, repeats: true)
     }
     
+    private func fadeInTextAnimation() {
+        UIView.animate(withDuration: 1.0) {
+            self.mainString.alpha = 1.0
+            self.subString.alpha = 1.0
+        }
+    }
+    
     @objc
     private func setProgress() {
         let duration: Float = 3.0
@@ -83,17 +119,28 @@ final class AnimationViewController: UIViewController {
         
         progressViews[currentIndex].setProgress(Float(progressViews[currentIndex].progress) + timeInterval / duration, animated: true)
         
+        mainString.text = mainStrings[currentIndex]
+        subString.text = subStrings[currentIndex]
+
         if Int(progressViews[currentIndex].progress * Float(steps)) >= steps {
             progressViews[currentIndex].setProgress(1.0, animated: true)
             currentIndex += 1
             if currentIndex < progressViews.count {
                 progressViews[currentIndex].setProgress(0.0, animated: false)
+                mainString.text = mainStrings[currentIndex]
+                subString.text = subStrings[currentIndex]
+                mainString.alpha = 0.0
+                subString.alpha = 0.0
+                fadeInTextAnimation()
             } else {
                 currentIndex = 0
                 for progressView in progressViews {
                     progressView.setProgress(0.0, animated: false)
                 }
                 setTimer()
+                mainString.alpha = 0.0
+                subString.alpha = 0.0
+                fadeInTextAnimation()
             }
         }
     }
--- Swift_Animation_Practice/FinalAssignment/Strings.swift
+++ Swift_Animation_Practice/FinalAssignment/Strings.swift
@@ -8,8 +8,17 @@
 import Foundation
 
 enum Strings {
-    static let first = "오늘 밤 12시부터\n 매일 이자 받을 수 있어요"
-    static let second = "목돈 모을 때\n 연 5% 받으며 모아요"
-    static let third = "긁을 때 마다\n 매일 최대 4,000원 받아요"
-    static let fourth = "비상금 필요할 때\n 5분만에 빌려요"
+    enum mainString {
+        static let first = "오늘 밤 12시부터\n매일 이자 받을 수 있어요"
+        static let second = "목돈 모을 때\n연 5% 받으며 모아요"
+        static let third = "긁을 때 마다\n매일 최대 4,000원 받아요"
+        static let fourth = "비상금 필요할 때\n5분만에 빌려요"
+    }
+    enum subString {
+        static let first = "한도 없이 연 2% (세전) 주는 통장"
+        static let second = "최대 연 5% (세전) 이자 주는 자유 적금"
+        static let third = "토스뱅크 체크카드 집중 캐시백"
+        static let fourth = "토스뱅크 신용대출"
+    }
+    
 }
